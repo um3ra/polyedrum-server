9e61d10559a2de8eb532a079e4ec2e6180e2e55f
um3ra <ereakvlad3@gmail.com>
2024-02-19 15:18:58 +0300

    added product search

--- src/main/java/com/shop/polyedrum/controller/ProductController.java
+++ src/main/java/com/shop/polyedrum/controller/ProductController.java
@@ -17,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -35,51 +36,10 @@ public class ProductController {
     ResponseEntity<Object> getProducts(@RequestParam(required = false) String sort,
                                        @RequestParam(required = false, defaultValue = "asc") String order,
                                        @RequestParam(required = false, defaultValue = "0") int pageNo,
-                                       @RequestParam(required = false, defaultValue = "10") int pageSize
-    ){
-        return ResponseHandler.responseBuilder("", HttpStatus.OK, productService.getAllProducts(pageNo, pageSize, sort, order));
-    }
-
-
-    @GetMapping({"{name}"})
-    public ResponseEntity<Object> getProductByName(@PathVariable String name){
-        return ResponseHandler.responseBuilder("", HttpStatus.OK, productService.getProductByName(name));
-    }
-
-    @PostMapping("/create")
-    public ResponseEntity<Object> createProduct(@RequestBody Product product){
-        return ResponseHandler.responseBuilder(productService.createProduct(product), HttpStatus.CREATED, "");
-    }
-
-    @PutMapping("/update/{id}")
-    public ResponseEntity<Object> updateProduct(@RequestBody Product product, @PathVariable Long id){
-        return ResponseHandler.responseBuilder(productService.updateProduct(product, id),
-                HttpStatus.OK,
-                "");
-    }
-
-    @DeleteMapping("{id}")
-    public ResponseEntity<Object> deleteProduct(@PathVariable Long id){
-        return ResponseHandler.responseBuilder(productService.deleteProduct(id), HttpStatus.OK, "");
-    }
-
-    @PostMapping
-    public ResponseEntity<Object> uploadImageToProduct(@RequestParam MultipartFile image, @RequestParam String product) throws IOException{
-        Product pr = productService.getProductByName(product);
-        ImageData uploadImage = imageDataService.uploadImage(image);
-        pr.setImageURL(path +"/api/v1/products/img/" + image.getOriginalFilename());
-        pr.setImage(uploadImage);
-        productService.updateProduct(pr, pr.getId());
-        return ResponseHandler.responseBuilder("image uploaded successfully", HttpStatus.OK, "");
-    }
-
-    @GetMapping("/img/{fileName}")
-    public ResponseEntity<Object> downloadProductImage(@PathVariable String fileName) throws IOException {
-        if (fileName.endsWith("png") || fileName.endsWith("jpg")){
-            byte[] imageData = imageDataService.downloadImage(fileName);
-            return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("image/png")).body(imageData);
-        }
-        throw new ApiException("format is incorrect", HttpStatus.BAD_REQUEST);
+                                       @RequestParam(required = false, defaultValue = "10") int pageSize,
+                                       @RequestParam(required = false) String destination
+    ) {
+        return ResponseHandler.responseBuilder("", HttpStatus.OK, productService.getAllProducts(pageNo, pageSize, sort, order, destination));
     }
 
     @GetMapping("/genre/{genreName}")
@@ -87,12 +47,28 @@ public class ProductController {
                                                         @RequestParam(required = false, defaultValue = "0") int pageNo,
                                                         @RequestParam(required = false, defaultValue = "10") int pageSize,
                                                         @RequestParam(required = false) String sort,
-                                                        @RequestParam(required = false, defaultValue = "asc") String order
-    ){
+                                                        @RequestParam(required = false, defaultValue = "asc") String order,
+                                                        @RequestParam(required = false) String destination
+    ) {
+        List<String> genreNames = new ArrayList<>();
+        genreNames.add(genreName);
         return ResponseHandler.responseBuilder("",
-                HttpStatus.OK, productService.findProductByGenre(pageNo, pageSize, genreName, sort, order));
+                HttpStatus.OK, productService.getAllProductsByGenreNames(pageNo, pageSize, sort, order, destination, genreNames));
     }
 
+    @GetMapping({"{name}"})
+    public ResponseEntity<Object> getProductByName(@PathVariable String name) {
+        return ResponseHandler.responseBuilder("", HttpStatus.OK, productService.getProductByName(name));
+    }
+
+    @GetMapping("/img/{fileName}")
+    public ResponseEntity<Object> downloadProductImage(@PathVariable String fileName) throws IOException {
+        if (fileName.endsWith("png") || fileName.endsWith("jpg")) {
+            byte[] imageData = imageDataService.downloadImage(fileName);
+            return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("image/png")).body(imageData);
+        }
+        throw new ApiException("format is incorrect", HttpStatus.BAD_REQUEST);
+    }
 
     @GetMapping("/author/{authorName}")
     public ResponseEntity<Object> getProductByAuthorName(@PathVariable String authorName,
@@ -100,37 +76,67 @@ public class ProductController {
                                                          @RequestParam(required = false, defaultValue = "10") int pageSize,
                                                          @RequestParam(required = false) String sort,
                                                          @RequestParam(required = false, defaultValue = "asc") String order
-    ){
+    ) {
         return ResponseHandler.responseBuilder("",
                 HttpStatus.OK, productService.findProductsByAuthor(pageNo, pageSize, authorName, sort, order));
     }
 
     @GetMapping("/author/all")
-    public ResponseEntity<Object> getAllProductAuthors(){
+    public ResponseEntity<Object> getAllProductAuthors() {
         return ResponseHandler.responseBuilder("", HttpStatus.OK, productService.getAllProductAuthors());
-    }
-
-    @PostMapping("{product}")
-    public ResponseEntity<Object> addProductToGenre(@PathVariable String product, @RequestParam String genre){
-        return ResponseHandler.responseBuilder(productService.addProductToGenre(product, genre),
-                HttpStatus.OK, "");
     }
 
     @GetMapping("category/{name}")
     public ResponseEntity<Object> getAllProductsByCategory(@RequestParam(required = false, defaultValue = "0") int pageNo,
-                                                 @RequestParam(required = false, defaultValue = "10") int pageSize,
-                                                 @RequestParam(required = false) String sort,
-                                                 @RequestParam(required = false, defaultValue = "asc") String order,
-                                                 @PathVariable String name
+                                                           @RequestParam(required = false, defaultValue = "10") int pageSize,
+                                                           @RequestParam(required = false) String sort,
+                                                           @RequestParam(required = false, defaultValue = "asc") String order,
+                                                           @RequestParam(required = false) String destination,
+                                                           @PathVariable String name
 
-    ){
+    ) {
         Category category = categoryService.getCategoryByName(name);
-        List<Genre> genres = category.getGenres();
-        return ResponseHandler.responseBuilder("", HttpStatus.OK, productService.getAllProductsByGenres(pageNo, pageSize, genres, sort, order));
+        List<String> genres = category.getGenres().stream().map(Genre::getName).toList();
+        return ResponseHandler.responseBuilder("", HttpStatus.OK, productService.getAllProductsByGenreNames(pageNo, pageSize, sort, order, destination, genres));
     }
 
+
+    @PostMapping("/create")
+    public ResponseEntity<Object> createProduct(@RequestBody Product product) {
+        return ResponseHandler.responseBuilder(productService.createProduct(product), HttpStatus.CREATED, "");
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> uploadImageToProduct(@RequestParam MultipartFile image, @RequestParam String product) throws IOException {
+        Product pr = productService.getProductByName(product);
+        ImageData uploadImage = imageDataService.uploadImage(image);
+        pr.setImageURL(path + "/api/v1/products/img/" + image.getOriginalFilename());
+        pr.setImage(uploadImage);
+        productService.updateProduct(pr, pr.getId());
+        return ResponseHandler.responseBuilder("image uploaded successfully", HttpStatus.OK, "");
+    }
+
+    @PostMapping("{product}")
+    public ResponseEntity<Object> addProductToGenre(@PathVariable String product, @RequestParam String genre) {
+        return ResponseHandler.responseBuilder(productService.addProductToGenre(product, genre),
+                HttpStatus.OK, "");
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Object> updateProduct(@RequestBody Product product, @PathVariable Long id) {
+        return ResponseHandler.responseBuilder(productService.updateProduct(product, id),
+                HttpStatus.OK,
+                "");
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable Long id) {
+        return ResponseHandler.responseBuilder(productService.deleteProduct(id), HttpStatus.OK, "");
+    }
+
+
     @PutMapping("{product}/genre/{genre}")
-    public ResponseEntity<Object> deleteGenreFromProduct(@PathVariable String product, @PathVariable String genre){
+    public ResponseEntity<Object> deleteGenreFromProduct(@PathVariable String product, @PathVariable String genre) {
         return ResponseHandler.responseBuilder(productService.deleteGenreFromProduct(product, genre), HttpStatus.OK, "");
     }
 }
--- src/main/java/com/shop/polyedrum/dao/ProductRepository.java
+++ src/main/java/com/shop/polyedrum/dao/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.shop.polyedrum.dao;
 
-import com.shop.polyedrum.domain.Genre;
 import com.shop.polyedrum.domain.Product;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,5 +12,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByTitle(String name);
     @Query("select distinct p.author from Product p")
     List<String> findAllAuthors();
-    List<Product> findAllByGenresIn(List<Genre> genres, Sort sort);
+    List<Product> findAllByTitleContainingIgnoreCase(String search, Sort sort);
+    List<Product> findAllByTitleContainingIgnoreCaseAndGenres_NameIn(String destination, List<String> genreNames, Sort sort);
 }
